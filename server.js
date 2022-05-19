f57980056e0dea282a5bb1142b280247679adf8c
Sebastian Alcina <micorreosar@gmail.com>
2022-05-18 22:44:28 -0500

    Changed selector in service

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const mysql = require('mysql')
 const util = require('util')
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  host: "sqlservertemp2.mysql.database.azure.com",
+  user: "jalcina@sqlservertemp2",
+  password: "Chucoclave123",
+  database: "mydb"
 })
 pool.query = util.promisify(pool.query)
 
